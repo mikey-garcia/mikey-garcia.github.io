d243588533b1f113be4409b5d185cadf2945b560
mikey-garcia <mgarcia75@gatech.edu>
2019-02-20 16:26:48 -0500

    Update #2
    Fixed buggy Responsiveness

--- ifDesktop.js
+++ ifDesktop.js
@@ -3,10 +3,11 @@ var isDesktop = window.screenX != 0 && !isTouchDevice() ? true : false;
 
 if(isDesktop){
     document.getElementById("buttons").style.display = "none";
+    
    	document.getElementById('canvas').style.width = "70%";
     document.getElementById('canvas').style.height = "auto";
     window.onload = function(){
      alert('Thank you for visiting my interactive resume!\nThis webpage is mobile optimized.\nHowever, you can still use the keyboard to move the character around (Arrow + A & B Keys)!');
     }
 
-}
+}//only works when isn't full screen?
